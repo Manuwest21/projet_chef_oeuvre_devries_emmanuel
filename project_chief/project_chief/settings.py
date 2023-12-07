287fc0019d5e5e555c37185ac93fb62c6ddc87a2
Manuwest21 <hammers178@hotmail.com>
2023-12-07 18:55:11 +0100

    new_vend

--- project_chief/project_chief/settings.py
+++ project_chief/project_chief/settings.py
@@ -183,5 +183,7 @@ STATIC_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = ["https://appmanuok2.azurewebsites.net"]
